f75419ea85fa91dbb1814760c48028716b5a65e7
Shekhar Shekhar <schandrasekhar@sapient.com>
2018-04-28 21:35:03 +0530

    initial commit

---
+++ hands-on/arm-templates/template-1/run.ps1
@@ -0,0 +1,3 @@
+.\deploy.ps1 -subscriptionId "170cf16c-77b6-4806-8a17-13b4e17d6187" `
+-resourceGroupName "az70533vm-rg" -resourceGroupLocation "centralindia" `
+-deploymentName "Deploy New VM";
---
+++ hands-on/scripts/customscript.ps1
@@ -0,0 +1,1 @@
+Get-Process | Out-File process_list.txt -Force
