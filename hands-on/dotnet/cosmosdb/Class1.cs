d312223c5ef5a4f558fa6c63d866379053be4ed1
Shekhar Shekhar <schandrasekhar@sapient.com>
2018-05-05 22:41:08 +0530

    dot net code for cosmos db added

---
+++ hands-on/dotnet/cosmosdb/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CosmosDBExplore
+{
+    public class Employee
+    {
+        [JsonProperty]
+        public string Id;
+
+        [JsonProperty]
+        public string Name;
+
+        [JsonProperty]
+        public string Designation;
+
+        [JsonProperty]
+        public int Age;
+
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        [JsonProperty]
+        public DateTime JoiningDate; 
+    }
+}
---
+++ hands-on/dotnet/cosmosdb/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace CosmosDBExplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpointurl = "https://az532cosmosdb.documents.azure.com:443/";
+            string primarykey = "f4JLBbWZTt6UJwVcPhvxn0sAkTRH2lMTINMCanUbsy5UNKABkmWp5FDtg4LkJ84m2bL4CB8AkJUkIvhQOdOB7A==";
+            DocumentClient client = new DocumentClient(new Uri(endpointurl), primarykey);
+            createDatabase(client);
+        }
+
+        private async static void createDatabase(DocumentClient client)
+        {
+            string db = "orgdb";
+            string collectionName = "empcoll";
+            await client.CreateDatabaseAsync(new Database { Id = db });
+            DocumentCollection collection = new DocumentCollection();
+            collection.Id = collectionName;
+            collection.PartitionKey.Paths.Add("/Designation");
+            await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(db), 
+                            collection, new RequestOptions { OfferThroughput = 2500});
+            await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(db, collectionName),
+                    new Employee
+                    {
+                        Id = "S0001",
+                        Designation = "Manager",
+                        Age = 36,
+                        JoiningDate = DateTime.UtcNow
+                    }
+            );
+
+            Document result = await client.ReadDocumentAsync(
+                UriFactory.CreateDocumentUri(db, collectionName, "S0001"),
+                new RequestOptions { PartitionKey = new PartitionKey("Manager") });
+
+            Employee readEmployee = (Employee)(dynamic)result;
+            Console.WriteLine(readEmployee.Age);
+            Console.ReadKey();
+        }
+    }
+}
